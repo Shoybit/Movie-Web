ae243f730e956565452a10e15d3024c558b65d9e
Md Shoyaib Islam <shoyaib105@gmail.com>
2026-05-15 00:12:20 +0600

    hero setup

---
+++ Src/Components/Hero.jsx
@@ -0,0 +1,15 @@
+export default function Hero() {
+  return (
+    <section className="h-[80vh] bg-black text-white flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-6xl font-bold mb-4">
+          Unlimited Movies
+        </h1>
+
+        <p className="text-gray-400">
+          Watch trending movies anytime.
+        </p>
+      </div>
+    </section>
+  );
+}
