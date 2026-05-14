10f39e9c0c9cb25029b2e2008e58020516ee8ca9
Md Shoyaib Islam <shoyaib105@gmail.com>
2026-05-15 01:12:04 +0600

    degain improved

--- Src/services/tmdb.js
+++ Src/services/tmdb.js
@@ -1,10 +1,27 @@
 const API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY;
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
-export const fetchTrendingMovies = async () => {
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export async function fetchTrendingMovies() {
   const res = await fetch(
     `${BASE_URL}/trending/movie/week?api_key=${API_KEY}`
   );
 
   return res.json();
-};
+}
+
+export async function fetchMovieDetails(id) {
+  const res = await fetch(
+    `${BASE_URL}/movie/${id}?api_key=${API_KEY}`
+  );
+
+  return res.json();
+}
+
+export async function fetchMovieVideos(id) {
+  const res = await fetch(
+    `${BASE_URL}/movie/${id}/videos?api_key=${API_KEY}`
+  );
+
+  return res.json();
+}
